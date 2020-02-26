b4d087ba45c7f466058037aa9b64895fc04092e1
Rian Tavares <rian.tavares@live.com>
2020-02-26 03:23:16 -0300

    terceiro desafio do projeto feito - Funcoes e estruturas de repeticao

---
+++ Desafio 1-3 Funcoes e estruturas de repeticao/buscaTech.js
@@ -0,0 +1,25 @@
+// Busca por tecnologia
+
+const usuarios = [
+    { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
+    { nome: "Jasmine", tecnologias: ["JavaScript", "CSS"] },
+    { nome: "Tuane", tecnologias: ["HTML", "Node.js"] }
+  ];
+
+
+  function checaSeUsuarioUsaCSS(usuario) {
+    for(let i=0; i<usuario.tecnologias.length; i++) {
+        if (usuario.tecnologias[i] == "CSS") {
+            return true
+        }
+    }
+    return false
+  }
+
+  for (let i = 0; i < usuarios.length; i++) {
+    const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(usuarios[i]);
+  
+    if (usuarioTrabalhaComCSS) {
+      console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`);
+    }
+  }
---
+++ Desafio 1-3 Funcoes e estruturas de repeticao/usuarios.js
@@ -0,0 +1,11 @@
+// Usuários e tecnologias
+
+const usuarios = [
+    { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
+    { nome: "Jasmine", tecnologias: ["JavaScript", "CSS"] },
+    { nome: "Tuane", tecnologias: ["HTML", "Node.js"] }
+  ];
+
+  for (let i=0; i < usuarios.length; i++) {
+      console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias[0]}, ${usuarios[i].tecnologias[1]}`)
+  }
